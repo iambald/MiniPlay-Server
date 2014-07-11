f4e1f4ae773c28a658ad4e911071d06906910cb2
Jeff Chen <jeffrey@cmu.edu>
2014-07-10 22:30:44 -0400

    Add newrelic and fix node-js warnings

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var express = require('express')
+require('newrelic');
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -6,30 +7,28 @@ var io = require('socket.io')(http);
 var PORT = process.env.PORT || 5000;
 
 function get_usertype(usertype) {
-    if (usertype === 'player') return 'controller';
-    if (usertype === 'controller') return 'player';
-    return '';
+  if (usertype === 'player') return 'controller';
+  if (usertype === 'controller') return 'player';
+  return '';
 }
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(socket){
-  socket.on('disconnect', function(){
+io.on('connection', function(socket) {
+  socket.on('disconnect', function() {
     console.log(socket.usertype + ' disconnected from ' + socket.room);
   });
   socket.on('room', function(info){
     if(socket.room) {
-        socket.leave(socket.room);
+      socket.leave(socket.room);
     }
-    console.log(info.client + ' joining room ' + info.room);
     socket.usertype = info.client;
     socket.room = info.room;
     socket.join(info.client + ' ' + info.room);
   });
 
   socket.on('data', function(data) {
-    var room = get_usertype(socket.usertype) + ' ' + socket.room;
-    io.to(room).emit('data', data);
+    io.to(get_usertype(socket.usertype) + ' ' + socket.room).emit('data', data);
   });
 });
 
---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['MiniPlay Server'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'e42fbd5a4bd3054d28e60ca9113e1823b3cc0eeb',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
